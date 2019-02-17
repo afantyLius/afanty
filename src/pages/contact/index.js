1ddcf6280a837930f12112bbd5b029efd7e1d6fa
afantyLius <1009721087@qq.com>
2019-02-17 15:44:16 +0800

    上传afanty官网代码

---
+++ src/pages/contact/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css'
+
+export default class Contact extends React.Component {
+
+    render(){
+        return(
+        <div className="contact-view">
+            <div className="contact-comment">
+                <p>地址：杭州市余杭区梦想小镇</p>
+                <p>网站：www.afanty.online.com</p>
+                <p>追梦，我们一直在路上</p>            
+            </div>
+        </div>)
+    }
+}
